5c6c8b7abe362972cd28a6b3ab3db6a357053292
Dulustan-N <dnikiforov@nes.ru>
2024-09-20 17:18:03 +0900

    added my template

---
+++ Debug/template.cpp
@@ -0,0 +1,33 @@
+#pragma GCC optimize("Ofast,unroll-loops")
+#include<bits/stdc++.h>
+using namespace std;
+
+using ll = long long; using ld = long double; 
+using pii = pair<int,int>; using pll = pair<ll,ll>;
+using vi = vector<int>; using vl = vector<ll>; using vc = vector<char>; using vb = vector<bool>;
+const int INF = 1e9+10; const ll INF_LL = 2e18; const ld PI = atan2(0,-1);
+#define F first
+#define S second
+#define all(x) (x).begin(), (x).end()
+#define forn(i,a,b) for(auto i=a; i<b; ++i)
+#define forr(i,a,b) for(auto i=a; i>b; --i)
+
+#ifdef LOCAL
+    #include "/home/dulustan/disk/projects/LIBR/Debug/debug.hpp"
+#else
+    #define deb(...) 0
+#endif
+
+void solve(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    #ifdef LOCAL
+        freopen("_in.txt", "r", stdin);
+        freopen("_out.txt", "w", stdout);
+    #endif    
+
+    solve();  
+}
