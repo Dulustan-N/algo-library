f94215264f96eb92997c4dc58014967e542a9aa3
Dulustan-N <dnikiforov@nes.ru>
2025-01-02 15:21:06 +0900

    pew

--- Strings/String-functions.cpp
+++ Strings/String-functions.cpp
@@ -24,11 +24,11 @@ vector<int> z(n);
     z[0] = n;
     int l=0, r=0; //[l,r)
     for(int i=1; i<n; ++i){
-        if(i<r) z[i] = min(z[i-l], r-i);
-        while(i+z[i]<=n && s[i+z[i]]==s[z[i]])
+        if(i < r) z[i] = min(z[i-l], r-i);
+        while(i+z[i] < n && s[i+z[i]] == s[z[i]])
             ++z[i];
-        if(i+z[i]>r)
-            l=i, r=i+z[i];
+        if(i+z[i] > r)
+            l = i, r = i+z[i];
     }
 }
 
